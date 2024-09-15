fb9fe96a3b83ae4f5862f3ee39c2ff579ee968d5
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-09-14 21:12:05 -0400

    added questions function

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
 const inquirer = require("inquirer");
 import { LoremIpsum } from "lorem-ipsum";
+import { type } from "os";
 const fs = require("fs");
 const generateMarkdown = require("./generateMarkdown.js");
+
+const questions = [
+    {
+        type: "input",
+        name: "Words",
+        message: "How many words would you like to generate?"
+
+    },
+    {
+        type: "input",
+        name: "Paragraphs",
+        message: "How many paragraphs would you like to generate?"
+    },
+    {
+        type: "input",
+        name: "Sentences",
+        message: "How many sentences would you like to generate?"
+    },
+    {
+        type: "input",
+        name: "Paragraphs",
+        message: "How many paragraphs would you like to generate?"
+    }
+];
+
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const lorem = new LoremIpsum({
+            wordsPerSentence: answers.Words,
+            sentencesPerParagraph: answers.Sentences
+        });
+        const content = lorem.generateParagraphs(answers.Paragraphs);
+        fs.writeFile("README.md", generateMarkdown(content), (err) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log("File created!");
+        });
+    });
+}
+
+init();
