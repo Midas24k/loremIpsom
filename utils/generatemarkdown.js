d1608d2c464549df36ae936c0c9972d5b54b7b80
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-09-18 22:20:17 -0400

    added comments

--- utils/generatemarkdown.js
+++ utils/generatemarkdown.js
@@ -1,4 +1,6 @@
+// Function to generate markdown content
 function generateMarkdown(content) {
+    // Return a formatted markdown string with the provided content
     return `# Lorem Ipsum Generator
 
     ## Generated Content
@@ -9,4 +11,5 @@ function generateMarkdown(content) {
     `;
 }
 
+// Export the generateMarkdown function as the default export
 export default generateMarkdown;
